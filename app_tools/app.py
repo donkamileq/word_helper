953eeac883302280b81cc655d7b565c568cc7a96
donkamileq <donkamileq@gmail.com>
2022-12-03 18:34:31 +0100

    Initial commit

---
+++ app_tools/app.py
@@ -0,0 +1,122 @@
+import logging
+import os
+
+from tools.word_helper_framenet import WordHelperFramenet
+from tools.word_helper_wordnet import WordHelperWordnet
+
+
+class MainApp:
+    @staticmethod
+    def display_title_bar():
+        os.system("clear")
+
+        print("\t=====================")
+        print("\t==== Simple Dict ====")
+        print("\t=====================")
+
+    @staticmethod
+    def get_user_choice():
+        print("\n[1] Get the definition of the word")
+        print("[2] Get sample sentences")
+        print("[3] Get the parts of speech")
+        print("[4] Get the synonyms")
+        print("[5] Check frames")
+        print("[6] Enter new word")
+        print("[q] Exit the program")
+
+        return input("What would you like to do ?: ")
+
+    @staticmethod
+    def get_user_choice_frame():
+        print("\n[1] Next frame")
+        print("[2] Previous frame")
+        print("[3] Quit frames")
+
+        return input("What would you like to do ?: ")
+
+    def menu(self):
+        choice = ""
+        self.display_title_bar()
+        user_word = input("Your word: ").strip()
+        while not user_word:
+            user_word = input("Please enter a word: ").strip()
+
+        while choice != "q":
+            word_helper_wordnet = WordHelperWordnet(word=user_word)
+            word_helper_framenet = WordHelperFramenet(word=user_word)
+            choice = self.get_user_choice()
+
+            if choice == "1":
+                words_definitions = word_helper_wordnet.get_the_definition()
+                for k, v in words_definitions.items():
+                    print(f"\n===Word===\n{k}\n=====Definition=====\n{v}\n")
+
+            elif choice == "2":
+                sentence_examples = word_helper_wordnet.get_a_sentence_example()
+                for k, v in sentence_examples.items():
+                    print(f"\n===Word===\n{k}\n=====Sentence Examples=====")
+                    for sen in range(len(v)):
+                        print(f"{sen+1}. {v[sen]}")
+
+            elif choice == "3":
+                parts_of_speech = word_helper_wordnet.get_the_parts_of_speech()
+                for k, v in parts_of_speech.items():
+                    print(f"\n===Word===\n{k}\n=====Part of speech=====\n{v}\n")
+
+            elif choice == "4":
+                try:
+                    synonyms = word_helper_wordnet.get_the_synonyms()
+                    for k, v in synonyms.items():
+                        print(f"\n===Synonyms - {k}===\n")
+                        for syn in range(len(v)):
+                            print(f"{syn + 1}. {v[syn]}")
+                except IndexError:
+                    print("There are no synonyms for this word")
+
+            elif choice == "5":
+                frame_choice = ""
+                num = 0
+                frame = word_helper_framenet.get_the_frame_of_word(num)
+                print(frame)
+                if frame == "There is no frame for the word":
+                    frame_choice = "3"
+
+                while frame_choice != "3":
+                    frame_choice = self.get_user_choice_frame()
+
+                    if frame_choice == "1":
+                        try:
+                            num += 1
+                            frame = word_helper_framenet.get_the_frame_of_word(num)
+                            print(frame)
+                        except IndexError:
+                            num = 0
+                            print("No more frames for a given word")
+
+                    elif frame_choice == "2":
+                        if num <= 0:
+                            num = 0
+                            print("There is no previous example before the first")
+                        else:
+                            num -= 1
+                        frame = word_helper_framenet.get_the_frame_of_word(num)
+                        print(frame)
+
+                    elif frame_choice == "3":
+                        pass
+                    else:
+                        print("\nI did not understand that choice.\n")
+
+            elif choice == "6":
+                user_word = "".strip()
+                while not user_word:
+                    user_word = input("Please enter a word: ").strip()
+
+            elif choice == "q":
+                print("\n\tHave a nice day !\n")
+                print("\t=====================")
+                print("\t==== Simple Dict ====")
+                print("\t=====================")
+                print("\t=======Gadwall=======")
+            else:
+                print("\nI did not understand that choice.\n")
---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+
+from app_tools.app import MainApp
+
+logging.basicConfig(
+    filename="app.log",
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+)
+
+
+if __name__ == "__main__":
+    main_app = MainApp()
+    main_app.menu()
